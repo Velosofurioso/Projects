6db302ad05ee333bdbe5c486bf6a11725343a7bd
l.brando815@gmail.com <l.brando815@gmail.com>
2020-06-23 14:12:27 -0300

    Add Projects Java

---
+++ Java - Spring/Api-Spring-MongoDB/src/main/java/com/estudos/workshopmongo/ApiSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringMongoDbApplication.class, args);
+	}
+
+}
