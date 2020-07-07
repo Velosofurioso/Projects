53c80401de918f28aabce09ebecffb140e16a69d
l.brando815@gmail.com <l.brando815@gmail.com>
2020-07-07 17:20:04 -0300

    Add project WebService-SpringBoot-Jpa

---
+++ Java - Spring/WebService-SpringBoot-Jpa/src/main/java/com/studies/webservicespringbootjpa/WebServiceSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.studies.webservicespringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ Java - Spring/WebService-SpringBoot-Jpa/src/test/java/com/studies/webservicespringbootjpa/WebServiceSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.webservicespringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
