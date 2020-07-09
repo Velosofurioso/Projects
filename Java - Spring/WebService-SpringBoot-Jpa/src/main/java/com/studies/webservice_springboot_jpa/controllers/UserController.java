13cba63e2f76e8b6ed5c3f90369ddfcfbce85966
l.brando815@gmail.com <l.brando815@gmail.com>
2020-07-09 19:51:54 -0300

    Add UserService

--- Java - Spring/WebService-SpringBoot-Jpa/src/main/java/com/studies/webservice_springboot_jpa/controllers/UserController.java
+++ Java - Spring/WebService-SpringBoot-Jpa/src/main/java/com/studies/webservice_springboot_jpa/controllers/UserController.java
@@ -1,19 +1,38 @@
 package com.studies.webservice_springboot_jpa.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studies.webservice_springboot_jpa.entities.User;
+import com.studies.webservice_springboot_jpa.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<User> findaAll(){
-		return ResponseEntity.ok().body(new User(Long.getLong("5"), "Lucas", "l.rbando815@gmail.com", "teste", "akaka"));
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> users = userService.findAll();
+		
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findaAll(@PathVariable(required = true) String id){
+		
+		User users = userService.findById(Long.parseLong(id));
+		
+		return ResponseEntity.ok().body(users);
 	}
 
 }
